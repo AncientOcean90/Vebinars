af9172d8ab839395b66aab013eed1c50dd9621e5
AncientOcean90 <ancientocean90@mail.ru>
2023-04-01 12:53:33 +0700

    Add Homework006

---
+++ Homework/Homework006/043/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double InputNumberWithMessage(string message)
+{
+    System.Console.Write(message);
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+double k1 = InputNumberWithMessage("к1: ");
+double b1 = InputNumberWithMessage("b1: ");
+
+double k2 = InputNumberWithMessage("k2: ");
+double b2 = InputNumberWithMessage("b2: ");
+
+double x = (b2 - b1)/(k1 - k2);
+double y = x * k1 + b1;
+
+System.Console.WriteLine("Точки пересечения:");
+System.Console.WriteLine($"x = {x}");
+System.Console.WriteLine($"y = {y}");
